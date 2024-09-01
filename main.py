116d120a9aa68226e6c9efcbf44c3b9f19c93056
Alex Akoopie <alexakoopie@gmail.com>
2024-08-31 18:44:19 -0700

    Updated main.py
    Cleaned up code.

--- main.py
+++ main.py
@@ -4,71 +4,83 @@ import random
 import time
 
 class Main:
-    def __init__(self, root: tk.Tk, diffuclty: int, num_choices: int):
-        self.characters = (
-            "あ", "い", "う", "え", "お",
-            "か", "き", "く", "け", "こ",
-            "さ", "し", "す", "せ", "そ",
-            "た", "ち", "つ", "て", "と",
-            "な", "に", "ぬ", "ね", "の",
-            "は", "ひ", "ふ", "へ", "ほ",
-            "ま", "み", "む", "め", "も",
-            "や", "ゆ", "よ",
-            "ら", "り", "る", "れ", "ろ",
-            "わ", "を",
-            "ん"
-        )
-        self.romaji = (
-            "a", "i", "u", "e", "o",
-            "ka", "ki", "ku", "ke", "ko",
-            "sa", "shi", "su", "se", "so",
-            "ta", "chi", "tsu", "te", "to",
-            "na", "ni", "nu", "ne", "no",
-            "ha", "hi", "fu", "he", "ho",
-            "ma", "mi", "mu", "me", "mo",
-            "ya", "yu", "yo",
-            "ra", "ri", "ru", "re", "ro",
-            "wa", "wo",
-            "n"
-        )
-        self.difficulty = difficulty
-        self.num_choices = num_choices
+    # Class variables - Static
+    hiragana = (
+        "あ", "い", "う", "え", "お",
+        "か", "き", "く", "け", "こ",
+        "さ", "し", "す", "せ", "そ",
+        "た", "ち", "つ", "て", "と",
+        "な", "に", "ぬ", "ね", "の",
+        "は", "ひ", "ふ", "へ", "ほ",
+        "ま", "み", "む", "め", "も",
+        "や", "ゆ", "よ",
+        "ら", "り", "る", "れ", "ろ",
+        "わ", "を",
+        "ん"
+    )
+    romaji = (
+        "a", "i", "u", "e", "o",
+        "ka", "ki", "ku", "ke", "ko",
+        "sa", "shi", "su", "se", "so",
+        "ta", "chi", "tsu", "te", "to",
+        "na", "ni", "nu", "ne", "no",
+        "ha", "hi", "fu", "he", "ho",
+        "ma", "mi", "mu", "me", "mo",
+        "ya", "yu", "yo",
+        "ra", "ri", "ru", "re", "ro",
+        "wa", "wo",
+        "n"
+    )
+    def __init__(self, root: tk.Tk, difficulty: int = 3, num_choices: int = 3):
+        # Instance variables
+        self.difficulty = difficulty    # Time limit
+        self.num_choices = num_choices  # Button count
         self.score = 0
         self.total = 0
+        self.character_set = []
+
+        # Builds app elements
         self.score_label = tk.Label(root, font=("Helvetica", 24), text="Accuracy: 0.00%")
         self.character_label = tk.Label(root, font=("Helvetica", 128))
         self.choice_frame = tk.Frame(root)
         self.choices = [tk.Button(self.choice_frame, font=("Helvetica", 24), height=1, width=4) for _button in range(num_choices)]
-        self.progress = ttk.Progressbar(root, orient='horizontal', length=200, mode='determinate')
-        self.progress['maximum'] = 1000 * difficulty
-        self.character_set = []
+        self.progress = ttk.Progressbar(root, maximum=1000 * difficulty, orient='horizontal', length=200, mode='determinate')
     
     def choose_character(self, expected, actual):
         if (expected == actual):
             self.score += 1
+        self.score_label.config(text=f"Accuracy: {(self.score / self.total) * 100:.2f}%")
         self.update_character()
 
     def update_character(self):
-        if self.total != 0:
-            self.score_label.config(text=f"Accuracy: {(self.score / self.total) * 100:.2f}%")
+        # Updates total
         self.total += 1
-        self.start_time = time.time()
+
+        # Grabs a new set of characters
         if not self.character_set:
             print("New Set")
-            self.character_set = list(zip(self.characters, self.romaji))
+            self.character_set = list(zip(self.hiragana, self.romaji))
             random.shuffle(self.character_set)
+        
+        # Grabs a new character from the set
         hiragana_character, romaji_character = self.character_set.pop()
-        choices = [romaji_character]
-        romajiCopy = list(self.romaji)
-        random.shuffle(romajiCopy)
-        while (len(choices) < num_choices):
-            random_character = romajiCopy.pop()
-            if (random_character != romaji_character):
-                choices.append(random_character)
+
+        # Gets choices to display for buttons
+        choices = set()
+        while len(choices) < self.num_choices - 1:
+            character = random.choice(self.romaji)
+            if character != romaji_character:
+                choices.add(character)
+        choices.add(romaji_character)
+        choices = list(choices)
         random.shuffle(choices)
+
+        # Configures app elements
         [self.choices[i].config(text=choices[i], command=lambda i=i: self.choose_character(romaji_character, choices[i])) for i in range(num_choices)]
         self.character_label.config(text=hiragana_character)
         self.progress['value'] = 1000 * self.difficulty # Resets progress bar
+
+        self.start_time = time.time()
         self.countdown()
         
     def countdown(self):
@@ -79,14 +91,15 @@ class Main:
             self.progress['value'] = remaining_time # Updates progress bar
             root.after(10, self.countdown)
         else:
+            self.score_label.config(text=f"Accuracy: {(self.score / self.total) * 100:.2f}%")
             self.update_character()
 
 # Create the main window
 root = tk.Tk()
 root.title("Flashcards")
 root.geometry("800x450")
-difficulty = 2 # 2 seconds
-num_choices = 3 # 3 buttons
+difficulty = 3
+num_choices = 3
 
 a = Main(root, difficulty, num_choices)
 a.score_label.pack(anchor="e")
